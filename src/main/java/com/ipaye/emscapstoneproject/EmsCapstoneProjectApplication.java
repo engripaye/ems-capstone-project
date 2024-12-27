d89ee7e9981ee4260885827ad9b2207bc1c539d5
Ipaye Babatunde <fineboyhakim@gmail.com>
2024-12-27 03:29:56 +0100

    init commit

---
+++ src/main/java/com/ipaye/emscapstoneproject/EmsCapstoneProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ipaye.emscapstoneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmsCapstoneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmsCapstoneProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ipaye/emscapstoneproject/EmsCapstoneProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ipaye.emscapstoneproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmsCapstoneProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
